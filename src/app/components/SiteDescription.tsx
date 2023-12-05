2acd5076a59e8edf6fca61cf352af361e35d3e07
stevenquintana <steve.e.quint@gmail.com>
2023-12-05 13:20:46 -0800

    Update site description text styles

--- src/app/components/SiteDescription.tsx
+++ src/app/components/SiteDescription.tsx
@@ -1,15 +1,22 @@
 import Image from 'next/image'
+import { fredoka } from '../utils/fonts'
 import siteCharacter from '../site-character.png'
 import styles from './SiteDescription.module.css'
 
 export default function SiteDescription() {
   return (
     <>
-    <div>
-      <h1>HQ Trivia</h1>
-      <p>Create your game</p>
-      <p>Join a game</p>
-      <p>Challenge your knowledge!</p>
+    <div className={ styles.titleContainer }>
+      <h1 className={ styles.title }>
+        <span className={ fredoka.className }>HQ Trivia</span>
+      </h1>
+      <p className={ styles.description }>
+        Create your game
+        <br />
+        Join a game
+        <br />
+        Challenge your knowledge!
+      </p>
     </div>
       <Image
         className={ styles.characterBkgdImg }
