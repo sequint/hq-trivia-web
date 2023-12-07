b2a9ca3eb5372d44539475e1d9ed6bbdff43f542
stevenquintana <steve.e.quint@gmail.com>
2023-12-07 11:18:32 -0800

    Implement temp function for checking if game exists beore implementing graphql

--- src/app/components/JoinGame.tsx
+++ src/app/components/JoinGame.tsx
@@ -1,7 +1,25 @@
+'use client'
+
+import { useState } from 'react';
 import Game from '../schemas/Game'
 import styles from './JoinGame.module.css'
 
 export default function JoinGame(params: { game: Game }) {
+  const [errorMessage, setErrorMessage] = useState<string>(' ');
+
+  // Temp vars for testing before imp graphql
+  let gameDoesNotExist: boolean = false;
+
+  function checkForGame(): void {
+    try {
+      if (gameDoesNotExist) throw new Error('This game is not active')
+
+      console.log('Go to game') // To be replaced with Link
+    }
+    catch (error: any) {
+      setErrorMessage(error.message)
+    }
+  }
   
   return (
     <div className={ styles.joinGameContainer }>
@@ -15,8 +33,15 @@ export default function JoinGame(params: { game: Game }) {
             placeholder='Enter a Game Name'
             defaultValue={ params.game.name }
           />
-          <button className={ styles.joinBtn }>Join as User</button>
-          <button className={ `${styles.joinBtn} ${styles.guestBtn}` }>Join as Guest</button>
+          <button 
+            className={ styles.joinBtn }
+            onClick={checkForGame}
+          >Join as User</button>
+          <button
+            className={ `${styles.joinBtn} ${styles.guestBtn}` }
+            onClick={checkForGame}
+          >Join as Guest</button>
+          <p>{ errorMessage }</p>
         </div>
       </div>
     </div>
