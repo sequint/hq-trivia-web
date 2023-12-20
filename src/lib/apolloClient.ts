2bc4d9805cbdac895b032dd6b50375a98aa90d2a
stevenquintana <steve.e.quint@gmail.com>
2023-12-20 07:09:27 -0800

    Implement apollo client

---
+++ src/lib/apolloClient.ts
@@ -0,0 +1,40 @@
+import { useMemo } from 'react'
+import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from '@apollo/client'
+'@apollo/client'
+
+let apolloClient: ApolloClient<NormalizedCacheObject>;
+
+function createApolloClient() {
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: new HttpLink({
+      uri: 'https://throbbing-field-370001.us-west-2.aws.cloud.dgraph.io/graphql'
+    }),
+    cache: new InMemoryCache()
+  })
+}
+
+export function initializeApollo(initialState = {}) {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  if (initialState) {
+    // Get existing cache, loaded during client side data fetching
+    const existingCache = _apolloClient.extract();
+
+    // Restore the cache using the data passed from
+    // getStaticProps/getServerSideProps combined with the existing cached data
+    _apolloClient.cache.restore({ ...existingCache, ...initialState });
+  }
+
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === "undefined") return _apolloClient;
+
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient;
+  return _apolloClient;
+}
+
+export function useApollo(initialState = {}) {
+  const store = useMemo(() => initializeApollo(initialState), [initialState]);
+  return store;
+}
