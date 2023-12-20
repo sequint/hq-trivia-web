101b085fd472f8152aa8b99208035008991754a8
stevenquintana <steve.e.quint@gmail.com>
2023-12-20 07:09:38 -0800

    Implement apollo client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
+'use client'
+
 import Link from 'next/link'
 import SiteDescription from './components/SiteDescription'
 import JoinGame from './components/JoinGame'
 import Game from './schemas/Game'
 import styles from './page.module.css'
+import { ApolloProvider } from '@apollo/client'
+import { useApollo } from '@/lib/apolloClient'
 
 export default function Home() {
   const tempUserName: string = 'sequint'
   const tempGame: Game = { id: 1, name: 'newgame' }
+  const apolloClient = useApollo()
 
   return (
-    <main className={ styles.homeContainer }>
-      <SiteDescription />
-      <JoinGame game={tempGame} />
-    </main>
+    <ApolloProvider client={ apolloClient }>
+      <main className={ styles.homeContainer }>
+        <SiteDescription />
+        <JoinGame game={tempGame} />
+      </main>
+    </ApolloProvider>
   )
 }
