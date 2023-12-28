78d220fea4170d2549b810358a257c546e4916b0
stevenquintana <steve.e.quint@gmail.com>
2023-12-28 11:30:47 -0800

    Remove description

--- src/app/home/components/SiteDescription.tsx
+++ src/app/home/components/SiteDescription.tsx
@@ -3,19 +3,10 @@ import styles from './SiteDescription.module.css'
 
 export default function SiteDescription() {
   return (
-    <>
     <div className={ styles.titleContainer }>
       <h1 className={ styles.title }>
         <span className={ fredoka.className }>HQ Trivia</span>
       </h1>
-      {/* <p className={ styles.description }>
-        Create your game
-        <br />
-        Join a game
-        <br />
-        Challenge your knowledge!
-      </p> */}
     </div>
-    </>
   )
 }
