8afb73d838a15820ec8a78cfbeff0d72afdf0cfa
stevenquintana <steve.e.quint@gmail.com>
2024-01-04 11:04:46 -0800

    Remove style from home logo component #14

--- src/app/home/components/HomeLogo.tsx
+++ src/app/home/components/HomeLogo.tsx
@@ -1,17 +1,13 @@
-import styles from './HomeLogo.module.css'
+import Image from 'next/image'
+import logo from '../../../images/gemer-logo.png'
 
 export default function HomeLogo() {
   return (
-    <div className={ styles.shapeContainer }>
-      <div className={ styles.leftShape }>
-        <div className={ styles.lTopHorzLine }></div>
-        <div className={ styles.lVertLine }></div>
-        <div className={ styles.lBotHorzLine }></div>
-      </div>
-      <div className={ styles.rightShape }>
-        <div className={ styles.rHorzLine }></div>
-        <div className={ styles.rVertLine }></div>
-      </div>
+    <div>
+      <Image
+        src={ logo }
+        alt='Site Logo'
+      />
     </div>
   )
 }
